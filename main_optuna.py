306c72de4e83a2a75e4a7d3f9c70d2c08b021cbb
jherasdo <jheras@iciq.es>
2025-04-28 11:23:24 +0200

    Changing License year to 2025

--- main_optuna.py
+++ main_optuna.py
@@ -1,5 +1,5 @@
 """
-Copyright (c) 2024 - Institute of Chemical Research of Catalonia (ICIQ)
+Copyright (c) 2025 - Institute of Chemical Research of Catalonia (ICIQ)
 
 This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
